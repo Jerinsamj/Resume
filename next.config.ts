a698be03c8e773f1cea943adb8f3cce23bc17f34
Jerinsamj <jerinsamjames98@outlook.com>
2025-07-30 13:32:02 -0400

    deploying

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type {NextConfig} from 'next';
 
+const repoName = 'Resume';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
